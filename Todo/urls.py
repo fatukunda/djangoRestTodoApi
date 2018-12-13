86311d1fe456a5496af9fd589082d5737b5ddb9f
Frank Atukunda <atukunda@MacBook-Pro-72.local>
2018-12-13 09:47:19 +0300

    Use ApiViews

--- Todo/urls.py
+++ Todo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import get_todos, get_single_todo
+from .views import TodoList, TodoDetails
 
 urlpatterns = [
-    path('todos/', get_todos, name='todos-list'),
-    path('todos/<int:id>/', get_single_todo, name='single-todo')
+    path('todos/', TodoList.as_view(), name='todos-list'),
+    path('todos/<int:id>/', TodoDetails.as_view(), name='todo-details')
 ]
--- Todo/views.py
+++ Todo/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Todo
+from .serializers import TodoSerializer
 
-def get_todos(request):
-    MAX_OBJECTS = 20
-    todos = Todo.objects.all()[: MAX_OBJECTS]
-    data = {
-        "results": list(todos.values('todo', 'completed', 'created_by'))
-    }
-    return JsonResponse(data)
+class TodoList(APIView):
+    def get(self, request):
+        todos = Todo.objects.all()[:20]
+        data = TodoSerializer(todos, many=True).data
+        return Response(data)
 
+class TodoDetails(APIView):
+    
+    def get(self, request, id):
+        todo = get_object_or_404(Todo, id = id)
+        data = TodoSerializer(todo).data
+        return Response(data)
 
-def get_single_todo(request, id):
-    todo = get_object_or_404(Todo, id = id)
-    data = {
-        "results": {
-            "todo": todo.todo,
-            "completed": todo.completed,
-            # "created_by": todo.created_by
-        }
-    }
-    return JsonResponse(data)
