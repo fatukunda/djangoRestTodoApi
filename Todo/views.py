2a17e193e2f9233f374c66002b05acbe81c660c8
Frank Atukunda <atukunda@MacBook-Pro-72.local>
2018-12-13 10:02:13 +0300

    Views with generics

--- Todo/views.py
+++ Todo/views.py
@@ -2,20 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Todo
 from .serializers import TodoSerializer
 
-class TodoList(APIView):
-    def get(self, request):
-        todos = Todo.objects.all()[:20]
-        data = TodoSerializer(todos, many=True).data
-        return Response(data)
+class TodoList(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+   
 
-class TodoDetails(APIView):
-    
-    def get(self, request, id):
-        todo = get_object_or_404(Todo, id = id)
-        data = TodoSerializer(todo).data
-        return Response(data)
+class TodoDetails(generics.RetrieveDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
